689000ba9a38273873936ce3c3472ad6813f28b7
jctest <jcmairot@gmail.com>
2021-01-07 15:08:16 +0100

    exo_ok

--- index.php
+++ index.php
@@ -12,9 +12,9 @@ try {
 
 if(!empty($_POST) && !empty($_FILES)){
 
-    echo '<pre>';
-    var_dump($_FILES);
-    echo '</pre>';
+    // echo '<pre>';
+    // var_dump($_FILES);
+    // echo '</pre>';
 
   
     define('MAX_SIZE', 500000);    // Taille max en octets du fichie
@@ -47,25 +47,25 @@ if(!empty($_POST) && !empty($_FILES)){
         //tableau des extension autorisé lors de l'upload
         $extension_autorisee = array('.jpeg','.jpg','.png','.gif','.webp');
 
-        // est-ce que fait parti du tableau ci dessus
-        if (in_array($file_extension, $extension_autorisee)){
-        // pour un autre if pour la taille ou dimension
-            if((filesize($_FILES['fichier']['tmp_name']) <= MAX_SIZE)){
+        // // est-ce que fait parti du tableau ci dessus
+        // if (in_array($file_extension, $extension_autorisee)){
+        // // pour un autre if pour la taille ou dimension
+        //     if((filesize($_FILES['fichier']['tmp_name']) <= MAX_SIZE)){
 
-                //pour déplacer du temp vers la destination
-                if(move_uploaded_file($file_tmp_name, $file_dest)){
+        //         //pour déplacer du temp vers la destination
+        //         if(move_uploaded_file($file_tmp_name, $file_dest)){
 
-                    echo 'Fichier envoyé avec succés';
+        //             echo 'Fichier envoyé avec succés';
 
-                }else{
-                    echo"Une erreur est survenue lors de l'envoi du fichier";
-                }
-            }else{
-                echo 'Erreur dans le poids !';
-            }
-        }else{
-            echo 'seuls les images avec les extensions jpeg, jpg, png, gif, webp sont autorisées';
-        }
+        //         }else{
+        //             echo"Une erreur est survenue lors de l'envoi du fichier";
+        //         }
+        //     }else{
+        //         echo 'Erreur dans le poids !';
+        //     }
+        // }else{
+        //     echo 'seuls les images avec les extensions jpeg, jpg, png, gif, webp sont autorisées';
+        // }
     
     
 
@@ -73,25 +73,68 @@ if(!empty($_POST) && !empty($_FILES)){
     // ********************************************************************************************************* 
 
     $firstName=strip_tags(trim($_POST['firstName']));
-        if(!preg_match("/^[a-zA-Z-']*$/",$firstName)){
-            echo 'le prenom de doit contenir que des lettre majuscule ou minuscule';
-        }
-
     $lastName=strip_tags(trim($_POST['lastName']));
-        if(!preg_match("/^[a-zA-Z-']*$/",$lastName)){
-            echo 'le nom de doit contenir que des lettre majuscule ou minuscule';
-        }
-
     $mailAdresse=strip_tags(trim($_POST['mailAdresse']));
     $mdp=sha1(trim($_POST['mdp']));
+
+// /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/
+
+        if(preg_match("/^[a-zA-Z-']*$/",$firstName)){
+            if(preg_match("/^[a-zA-Z-']*$/",$lastName)){
+                if (preg_match("/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/",$mailAdresse)) {
+
+                    // *********************** import img *********************
+
+                                // est-ce que fait parti du tableau ci dessus
+                    if (in_array($file_extension, $extension_autorisee)){
+                        // pour un autre if pour la taille ou dimension
+                            if((filesize($_FILES['fichier']['tmp_name']) <= MAX_SIZE)){
+                
+                                //pour déplacer du temp vers la destination
+                                if(move_uploaded_file($file_tmp_name, $file_dest)){
+                
+                                    echo 'Fichier envoyé avec succés';
+                
+                                }else{
+                                    echo"Une erreur est survenue lors de l'envoi du fichier";
+                                }
+                            }else{
+                                echo 'Erreur dans le poids !';
+                            }
+                        }else{
+                            echo 'seuls les images avec les extensions jpeg, jpg, png, gif, webp sont autorisées';
+                        }
+                        // ******************************* FIN IMPORT IMG **********************************************
+
+
+                    //après verification du nom prenom mail on execute la requete
+                    $request=$pdo->prepare("INSERT INTO user(firstName,lastName,mailAdresse,mdp,fichier) VALUES (?,?,?,?,?)");
+                    $request->execute(array($firstName, $lastName, $mailAdresse, $mdp,$file_dest)); 
+                    
+                    echo 'et User enregistré';
+
+                }else{
+                    echo"l'adresse mail n'est pas valide";
+                }
+            }else{
+                echo 'le nom de doit contenir que des lettre majuscule ou minuscule';
+            }  
+        }else{
+            echo 'le prenom ne doit contenir que des lettres majuscule ou minuscule';
+        }
+
+   
+        
+
+    
 
     //$name = test_input($_POST["name"]);
     // if (!preg_match("/^[a-zA-Z-']*$/",$name)) {
     //   $nameErr = "Only letters and white space allowed";
     // } 
 
-    $request=$pdo->prepare("INSERT INTO user(firstName,lastName,mailAdresse,mdp,fichier) VALUES (?,?,?,?,?)");
-    $request->execute(array($firstName, $lastName, $mailAdresse, $mdp,$file_dest));     
+    // $request=$pdo->prepare("INSERT INTO user(firstName,lastName,mailAdresse,mdp,fichier) VALUES (?,?,?,?,?)");
+    // $request->execute(array($firstName, $lastName, $mailAdresse, $mdp,$file_dest));     
 }
 
 
@@ -103,12 +146,12 @@ if(!empty($_POST) && !empty($_FILES)){
     <label>Nom</label><br>
     <input type="text" name="lastName"><br>
     <label>E-mail</label><br>
-    <input type="email" name="mailAdresse"><br>
+    <input type="text" name="mailAdresse"><br>
     <label>Mot de passe</label><br>
     <input type="password" name="mdp"><br>
     <!-- pour l'image -->
     <input name="fichier" type="file" />
     <!-- <input type="file" name="avatar"> <br>-->
-    <input type="submit" value="enregistrer" name="submit">
+    <input type="submit" value="enregistrer" name="subm">
 </form>
 
